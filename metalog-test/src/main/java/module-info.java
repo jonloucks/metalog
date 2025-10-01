3b8f4ea228f2fc6e3da054be126bc8d64d08a235
Jon Loucks <j.loucks@comcast.net>
2025-10-01 07:19:45 -0400

    Publishing Code Coverage and Source Documentation to gh-pages

--- metalog-test/src/main/java/module-info.java
+++ metalog-test/src/main/java/module-info.java
@@ -2,9 +2,9 @@ module io.github.metalog.metalog.test {
     requires transitive io.github.jonloucks.metalog.api;
     requires transitive io.github.jonloucks.contracts.api;
     requires transitive io.github.jonloucks.contracts.test;
-    requires org.junit.jupiter.api;
-    requires org.mockito.junit.jupiter;
-    requires org.mockito;
+    requires transitive org.junit.jupiter.api;
+    requires transitive org.mockito.junit.jupiter;
+    requires transitive org.mockito;
     
     opens io.github.jonloucks.metalog.test to org.junit.platform.commons;
     exports io.github.jonloucks.metalog.test;
---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * Includes all default components of the Metalog library needed for a working deployment.
+ */
+module io.github.jonloucks.metalog {
+    requires io.github.jonloucks.metalog.api;
+    requires io.github.jonloucks.metalog.impl;
+    
+    exports io.github.jonloucks.metalog;
+}
