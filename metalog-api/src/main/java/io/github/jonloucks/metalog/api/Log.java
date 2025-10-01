bf33f81c739ee9781b9fce08c177981f9dca9935
jonloucks <j.loucks@comcast.net>
2025-10-01 07:26:28 -0400

    Publishing Code Coverage and Source Documentation to gh-pages (#3)

--- metalog-api/src/main/java/io/github/jonloucks/metalog/api/Log.java
+++ metalog-api/src/main/java/io/github/jonloucks/metalog/api/Log.java
@@ -2,6 +2,9 @@ package io.github.jonloucks.metalog.api;
 
 import java.util.function.Supplier;
 
+/**
+ * The invoked when the log message text is actually needed
+ */
 @FunctionalInterface
 public interface Log extends Supplier<CharSequence> {
 }
---
+++ src/main/java/io/github/jonloucks/metalog/Stub.java
@@ -0,0 +1,21 @@
+package io.github.jonloucks.metalog;
+
+import io.github.jonloucks.contracts.api.Checks;
+import io.github.jonloucks.contracts.api.GlobalContracts;
+
+/**
+ * A placeholder class to make sure dependencies are correct for api and implementation.
+ */
+public final class Stub {
+ 
+    private Stub() {
+        throw new AssertionError("Illegal constructor");
+    }
+    
+    /**
+     * Validates basic functionality.
+     */
+    public static void validate() {
+        Checks.validateContracts(GlobalContracts.getInstance());
+    }
+}
